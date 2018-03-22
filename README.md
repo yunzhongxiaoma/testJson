070f24ac23b586a03ddfc095ed755fc732fcb938
yunzhongxiaoma <i_need_you0.0@qq.com>
2018-03-22 22:36:12 +0800

    add testFastJson

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # testJson
+ demo程序 
+把对象、内嵌对象、Map对象、Set集合对象、list集合对象转json串
+对fastjson-1.2.8.jar和gson-2.2.4.jar两种jar的 简单api方法使用
--- src/com/baizhi/test/TestFastJson.java
+++ src/com/baizhi/test/TestFastJson.java
@@ -1,5 +1,76 @@
 package com.baizhi.test;
 
-public class TestFastJson {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
+import org.junit.Test;
+
+import com.alibaba.fastjson.JSON;
+import com.baizhi.bean.Address;
+import com.baizhi.bean.User;
+
+public class TestFastJson {
+    // 对象转json串
+    @Test
+    public void testBean() {
+        User u = new User("zhangsan", "123456", 18, null);
+        
+        String jsonString = JSON.toJSONString(u);
+        System.out.println(jsonString.toString());
+    }
+    
+
+    // 内嵌对象转json
+    @Test
+    public void testNeiQianBean() {
+        User u = new User("zhangsan", "123456", 18, null);
+        Address address = new Address("北京", "中关村");
+        u.setAddress(address);
+
+        String jsonString = JSON.toJSONString(u);
+        System.out.println(jsonString.toString());
+    }
+
+    // Map对象转json
+    @Test
+    public void testMap() {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "小黄");
+        map.put(2, "小黑");
+
+        String jsonString = JSON.toJSONString(map);
+        System.out.println(jsonString.toString());
+    }
+
+    // Set集合对象转Json
+    @Test
+    public void testSet() {
+        HashSet<User> set = new HashSet<>();
+        User u = new User("zhangsan", "123456", 18, null);
+        User u1 = new User("zhangsan1", "123456", 18, null);
+        User u2 = new User("zhangsan2", "123456", 18, null);
+        set.add(u);
+        set.add(u1);
+        set.add(u2);
+
+        String jsonString = JSON.toJSONString(set);
+        System.out.println(jsonString.toString());
+    }
+    
+    // list集合对象转json
+    @Test
+    public void testList() {
+        User u = new User("zhangsan", "123456", 18, null);
+        User u1 = new User("zhangsan1", "123456", 18, null);
+        User u2 = new User("zhangsan2", "123456", 18, null);
+        ArrayList<User> list = new ArrayList<User>();
+        list.add(u);
+        list.add(u1);
+        list.add(u2);
+
+        String jsonString = JSON.toJSONString(list);
+        System.out.println(jsonString.toString());
+    }
 }
